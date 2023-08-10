39c77026c4640c600eb7ffab5c265d4d1300d029
giorgi568 <mateshvili02@mail.ru>
2023-08-10 22:23:22 +0400

    move function

--- knightsTravails.js
+++ knightsTravails.js
@@ -1,1 +1,14 @@
-knightsTravails.js
+class gameBoard {
+	constructor() {
+		this.board = [];
+		for(let i = 0; i < 8; i++){
+			this.board[i] = [];
+			for(let j = 0; j < 8; j++){
+				this.board[i][j] = `${i}, ${j}`
+			}
+		}
+	}
+}
+
+let k = new gameBoard();
+console.log(k.board);
